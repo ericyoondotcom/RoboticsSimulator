bc4028122459617f56509f772b9c0536a610d6b8
Eric <emjyoon@gmail.com>
2020-02-27 21:11:46 -0800

    Make field, add controllers

---
+++ Vex Simulator VR/Assets/_Scripts/BasicGrabbable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicGrabbable : MonoBehaviour, IGrabbable
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnGrabStart()
+    {
+
+    }
+    public void OnGrabEnd()
+    {
+
+    }
+
+}
---
+++ Vex Simulator VR/Assets/_Scripts/OculusGrab.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+// Thanks, https://circuitstream.com/blog/grab-oculus/
+[RequireComponent(typeof(Collider))]
+public class OculusGrab : MonoBehaviour
+{
+
+    public List<(IGrabbable grabbable, GameObject gameObject, Rigidbody rb)> inRadius;
+    public (IGrabbable grabbable, GameObject gameObject, Rigidbody rb) objectInHand;
+
+    Transform oldParent;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        IGrabbable grabbable = other.GetComponent<IGrabbable>();
+        if (grabbable == null) return;
+
+        inRadius.Add((grabbable, other.gameObject, other.attachedRigidbody));
+
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        IGrabbable grabbable = other.GetComponent<IGrabbable>();
+        if (grabbable == null) return;
+
+        bool removed = inRadius.Remove((grabbable, other.gameObject, other.attachedRigidbody));
+        if (removed)
+        {
+            if(grabbable == objectInHand.grabbable)
+            {
+                ReleaseObject();
+            }
+        }
+    }
+
+    (IGrabbable grabbable, GameObject gameObject, Rigidbody rb) NearestGrabbable()
+    {
+        return inRadius.Aggregate((i1, i2) =>
+            Vector3.Distance(transform.position, i1.gameObject.transform.position) > Vector3.Distance(transform.position, i2.gameObject.transform.position) ? i1 : i2
+        );
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger") > 0.2f && inRadius.Count > 0)
+        {
+            GrabObject();
+        }
+
+        if (Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger") < 0.2f && inRadius.Count > 0)
+        {
+            ReleaseObject();
+        }
+    }
+
+    public void GrabObject()
+    {
+        objectInHand = NearestGrabbable();
+        objectInHand.grabbable.OnGrabStart();
+        oldParent = objectInHand.gameObject.transform.parent;
+        objectInHand.gameObject.transform.SetParent(transform);
+        objectInHand.rb.isKinematic = true;
+    }
+
+    private void ReleaseObject()
+    {
+        objectInHand.grabbable.OnGrabEnd();
+        objectInHand.rb.isKinematic = false;
+        objectInHand.gameObject.transform.SetParent(oldParent);
+        objectInHand = (null, null, null);
+    }
+
+}
