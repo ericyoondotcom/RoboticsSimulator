68264996c5f0700cba6acd1906cbd2f76bc196c9
Eric <emjyoon@gmail.com>
2020-02-29 19:47:49 -0800

    Grabbing working!
    I already did this but in a detached HEAD so my changes got destroyed yay

---
+++ Vex Simulator VR/Assets/_Scripts/Grabbable.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cakeslice;
+
+using static Enums;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Grabbable : MonoBehaviour
+{
+    public Outline outline;
+
+    [System.NonSerialized]
+    public OculusGrab grabber;
+
+    Dictionary<Side, bool> isClosest;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        outline.enabled = false;
+        isClosest = new Dictionary<Side, bool>()
+        {
+            {Side.Left, false },
+            {Side.Right, false }
+        };
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OculusGrab grabScript = other.GetComponent<OculusGrab>();
+        if(grabScript == null)
+        {
+            return;
+        }
+        grabScript.GrabbableEnteredRange(this);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OculusGrab grabScript = other.GetComponent<OculusGrab>();
+        if (grabScript == null)
+        {
+            return;
+        }
+        grabScript.GrabbableOutOfRange(this);
+    }
+
+    public void OnClosestStateChanged(bool closest, Side hand)
+    {
+        isClosest[hand] = closest;
+        outline.enabled = isClosest[Side.Left] || isClosest[Side.Right];
+    }
+
+    public void InternalOnGrabStart(OculusGrab grabber)
+    {
+        this.grabber = grabber;
+        rb.isKinematic = true;
+    }
+    public void InternalOnGrabEnd(OculusGrab grabber)
+    {
+        this.grabber = null;
+        rb.isKinematic = false;
+    }
+
+    public virtual void OnGrabStart() { }
+    public virtual void OnGrabEnd() { }
+
+}
--- Vex Simulator VR/Assets/_Scripts/OculusGrab.cs
+++ Vex Simulator VR/Assets/_Scripts/OculusGrab.cs
@@ -3,76 +3,107 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+using static Enums;
 // Thanks, https://circuitstream.com/blog/grab-oculus/
 [RequireComponent(typeof(Collider))]
 public class OculusGrab : MonoBehaviour
 {
 
-    public List<(IGrabbable grabbable, GameObject gameObject, Rigidbody rb)> inRadius;
-    public (IGrabbable grabbable, GameObject gameObject, Rigidbody rb) objectInHand;
+    public Side hand;
+
+    public List<(Grabbable grabbable, GameObject gameObject)> inRadius;
+    public (Grabbable grabbable, GameObject gameObject) objectInHand;
+
+    [System.NonSerialized]
+    public Grabbable previousClosest = null;
 
     Transform oldParent;
 
+    private void Start()
+    {
+        inRadius = new List<(Grabbable grabbable, GameObject gameObject)>();
+    }
+
     public void OnTriggerEnter(Collider other)
     {
-        IGrabbable grabbable = other.GetComponent<IGrabbable>();
+        Grabbable grabbable = other.GetComponent<Grabbable>();
         if (grabbable == null) return;
 
-        inRadius.Add((grabbable, other.gameObject, other.attachedRigidbody));
+        inRadius.Add((grabbable, other.gameObject));
 
 
     }
 
-    public void OnTriggerExit(Collider other)
+    public void GrabbableEnteredRange(Grabbable grabbable)
     {
-        IGrabbable grabbable = other.GetComponent<IGrabbable>();
-        if (grabbable == null) return;
-
-        bool removed = inRadius.Remove((grabbable, other.gameObject, other.attachedRigidbody));
-        if (removed)
-        {
-            if(grabbable == objectInHand.grabbable)
-            {
-                ReleaseObject();
-            }
-        }
+        inRadius.Add((grabbable, grabbable.gameObject));
+        print(inRadius);
     }
 
-    (IGrabbable grabbable, GameObject gameObject, Rigidbody rb) NearestGrabbable()
+    public void GrabbableOutOfRange(Grabbable grabbable)
     {
+        bool removed = inRadius.Remove((grabbable, grabbable.gameObject));
+    }
+
+    (Grabbable grabbable, GameObject gameObject) NearestGrabbable()
+    {
+        if (inRadius.Count == 0) return (null, null);
         return inRadius.Aggregate((i1, i2) =>
-            Vector3.Distance(transform.position, i1.gameObject.transform.position) > Vector3.Distance(transform.position, i2.gameObject.transform.position) ? i1 : i2
+            Vector3.Distance(transform.position, i1.gameObject.transform.position) < Vector3.Distance(transform.position, i2.gameObject.transform.position) ? i1 : i2
         );
     }
 
     void Update()
     {
-        if ((Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger") > 0.2f || Input.GetMouseButton(0)) && inRadius.Count > 0)
+        float grabAxis;
+#if UNITY_EDITOR
+        grabAxis = Input.GetMouseButton(0) ? 1 : 0;
+#else
+        grabAxis = Input.GetAxis(
+            hand == Side.Left ?
+            "Oculus_CrossPlatform_PrimaryHandTrigger" :
+            "Oculus_CrossPlatform_SecondaryHandTrigger"
+        );
+#endif
+        if (grabAxis > 0.2f && inRadius.Count > 0)
         {
             GrabObject();
         }
 
-        if ((Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger") < 0.2f && !Input.GetMouseButton(0)) && inRadius.Count > 0)
+        if (grabAxis < 0.2f)
         {
             ReleaseObject();
         }
+
+        Grabbable closest = NearestGrabbable().grabbable;
+        if(previousClosest != null) previousClosest.OnClosestStateChanged(false, hand);
+        if(closest != null) closest.OnClosestStateChanged(closest != objectInHand.grabbable, hand);
+        previousClosest = closest;
+        
     }
 
     public void GrabObject()
     {
-        objectInHand = NearestGrabbable();
-        objectInHand.grabbable.OnGrabStart();
+        if (objectInHand.gameObject != null) return;
+        var nearest = NearestGrabbable();
+        if(nearest.grabbable.grabber != null)
+        {
+            nearest.grabbable.grabber.ReleaseObject();
+        }
+        print("Picked up");
+        objectInHand = nearest;
+        objectInHand.grabbable.InternalOnGrabStart(this);
         oldParent = objectInHand.gameObject.transform.parent;
         objectInHand.gameObject.transform.SetParent(transform);
-        objectInHand.rb.isKinematic = true;
     }
 
     private void ReleaseObject()
     {
-        objectInHand.grabbable.OnGrabEnd();
-        objectInHand.rb.isKinematic = false;
+        if (objectInHand.grabbable == null) return;
+        print("Released");
+        objectInHand.grabbable.InternalOnGrabEnd(this);
         objectInHand.gameObject.transform.SetParent(oldParent);
-        objectInHand = (null, null, null);
+        objectInHand = (null, null);
     }
 
 }
