147251b8b4cf5cb5ae8f4052ee6d93e28b0347c2
Eric <emjyoon@gmail.com>
2020-03-06 22:28:03 -0800

    Try using parenting; will try hinge joints

---
+++ Vex Simulator VR/Assets/_Scripts/CopyRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Constantly sets the rotation of an object.
+/// </summary>
+public class CopyRotation : MonoBehaviour
+{
+    public Transform relativeTo;
+    public bool setX;
+    public bool setY;
+    public bool setZ;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+		transform.rotation = Quaternion.Euler(
+            setX ? relativeTo.transform.eulerAngles.x : transform.eulerAngles.x,
+            setY ? relativeTo.transform.eulerAngles.y : transform.eulerAngles.y,
+            setZ ? relativeTo.transform.eulerAngles.z : transform.eulerAngles.z
+        );
+    }
+}
